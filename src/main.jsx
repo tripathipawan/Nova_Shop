a83a4a0d23b6db86c360ac70c007d5c3409ec1e0
Pawan Tripathi <pawantripathi.ind@gmail.com>
2026-06-09 14:17:18 +0530

    Fixed sum bugs

--- src/main.jsx
+++ src/main.jsx
@@ -14,14 +14,17 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
-const CLERK_PROXY_URL = `${window.location.origin}/clerk-proxy`
+const isProduction = window.location.protocol === 'https:'
+const CLERK_PROXY_URL = isProduction
+  ? `${window.location.origin}/clerk-proxy`
+  : undefined
 
 createRoot(document.getElementById('root')).render(
   <DataProvider>
     <CartProvider>
       <ClerkProvider
         publishableKey={PUBLISHABLE_KEY}
-        proxyUrl={CLERK_PROXY_URL}
+        {...(CLERK_PROXY_URL ? { proxyUrl: CLERK_PROXY_URL } : {})}
         afterSignOutUrl="/"
       >
         <App />
